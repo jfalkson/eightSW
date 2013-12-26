12dd859f767709a62c49579877b9080b70210b98
jfalkson <jfalkson@gmail.com>
2013-12-25 21:10:09 -0500

    votes table

--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -1,6 +1,7 @@
 class Recommendation < ActiveRecord::Base
 belongs_to :user
 has_many :comments
+has_many :votes
 #validates :link, presence: true
 #validates :rec_type, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,7 @@ class User < ActiveRecord::Base
   validates :email, presence: true, uniqueness: true
   has_many :recommendations, dependent: :destroy
   has_many :comments
+  has_many :votes
 
 
 
